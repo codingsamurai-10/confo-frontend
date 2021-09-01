4d04d2555738d2aea428ac5f4814c2fd93ab70db
Aditya Rana <official.adityarana01@gmail.com>
2021-09-02 04:17:34 +0530

    Init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Form from "./components/Form/Form";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
 }
 
 export default App;
