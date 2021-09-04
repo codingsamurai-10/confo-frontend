6f0d08997184f466b3fd53eefabba4dbe7d9e7f0
Jashan Devgan <jashandevgan@gmail.com>
2021-09-04 16:48:31 +0530

    Added voiceInput option to Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ConversationalForm } from "conversational-form";
+import MicrophoneInputConfig from "./MicrophoneInputConfig";
 
 export default class Form extends React.Component {
   constructor(props) {
@@ -16,6 +17,7 @@ export default class Form extends React.Component {
       options: {
         submitCallback: this.submitCallback,
         flowStepCallback: this.props.flowStepCallback,
+        microphoneInput: MicrophoneInputConfig
       },
       tags: this.props.formFields,
     });
