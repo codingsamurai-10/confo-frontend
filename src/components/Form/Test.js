a07ce629f61fc648fdd213923e3331ae77a4d517
Aditya Rana <official.adityarana01@gmail.com>
2021-09-21 21:07:56 +0530

    Moved Test.js inside form

---
+++ src/components/Form/Test.js
@@ -0,0 +1,19 @@
+import Uppy from "@uppy/core";
+import React from "react";
+import Tus from "@uppy/tus";
+import "@uppy/core/dist/style.css";
+import "@uppy/dashboard/dist/style.css";
+import { DashboardModal, useUppy } from "@uppy/react";
+import GoogleDrive from "@uppy/google-drive";
+
+export default function Test() {
+  const uppy = useUppy(() => {
+    return new Uppy()
+      .use(GoogleDrive, {
+        companionUrl: "https://companion.uppy.io/connect/drive/callback",
+      })
+      .use(Tus, { endpoint: "https://tusd.tusdemo.net/files" });
+  });
+
+  return <DashboardModal uppy={uppy} open={true} plugins={["GoogleDrive"]} />;
+}
