3124529c76dbb7a9d28af18232a06fa1115dbad5
Aditya Rana <official.adityarana01@gmail.com>
2021-09-21 21:15:37 +0530

    Opening file upload modal dynamically

--- src/components/Form/FileUpload.js
+++ src/components/Form/FileUpload.js
@@ -6,7 +6,7 @@ import "@uppy/dashboard/dist/style.css";
 import { DashboardModal, useUppy } from "@uppy/react";
 import GoogleDrive from "@uppy/google-drive";
 
-export default function FileUpload() {
+export default function FileUpload(props) {
   const uppy = useUppy(() => {
     return new Uppy()
       .use(GoogleDrive, {
@@ -15,5 +15,7 @@ export default function FileUpload() {
       .use(Tus, { endpoint: "https://tusd.tusdemo.net/files" });
   });
 
-  return <DashboardModal uppy={uppy} open={true} plugins={["GoogleDrive"]} />;
+  return (
+    <DashboardModal uppy={uppy} open={props.open} plugins={["GoogleDrive"]} />
+  );
 }
