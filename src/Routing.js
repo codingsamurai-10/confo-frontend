461fb7b7eb79e883bc43f237235517ce2e5a3b37
Aditya Rana <official.adityarana01@gmail.com>
2021-09-27 23:54:19 +0530

    Added route for responses

--- src/Routing.js
+++ src/Routing.js
@@ -4,6 +4,7 @@ import AdminForm from "./components/AdminForm";
 import CreatedFormsPage from "./components/CreatedFormsPage";
 import FormPage from "./components/FormPage/FormPage";
 import HomePage from "./components/HomePage";
+import ResponsePage from "./components/ResponsePage";
 
 const Routing = () => {
   return (
@@ -12,6 +13,7 @@ const Routing = () => {
         <Route exact path="/" component={HomePage} />
         <Route exact path="/FormPage/:id" component={FormPage} />
         <Route exact path="/CreatedForms" component={CreatedFormsPage} />
+        <Route exact path="/responses/:id" component={ResponsePage} />
         <Route exact path="/AdminForm" component={AdminForm} />
       </Switch>
     </Fade>
