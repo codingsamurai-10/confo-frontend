64fb76b8f9422a870aa456312a594ad920361853
Aditya Rana <official.adityarana01@gmail.com>
2021-09-27 23:01:26 +0530

    Created forms created page

--- src/components/CreatedFormsPage.js
+++ src/components/CreatedFormsPage.js
@@ -1,9 +1,25 @@
+import axios from "axios";
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function CreatedFormsPage() {
+  const [formsList, setFormsList] = React.useState(null);
+  React.useEffect(() => {
+    axios.get("http://localhost:5000/api/form/getall").then((res) => {
+      setFormsList(res.data);
+    });
+  }, []);
   return (
     <div>
       <h2>Your forms</h2>
+      <ul>
+        {formsList &&
+          formsList.map((form) => (
+            <li component={Link} to={`/${form._id}`}>
+              {form.formName}
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
