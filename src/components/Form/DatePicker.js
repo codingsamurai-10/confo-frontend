50f96380f266b508e46426fd74ade9d210821d09
Jashan Devgan <jashandevgan@gmail.com>
2021-09-28 21:19:29 +0530

    Added open/close handlers

--- src/components/Form/DatePicker.js
+++ src/components/Form/DatePicker.js
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
 import DateFnsUtils from "@date-io/date-fns";
 import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import { Dialog } from "@material-ui/core";
 
-const DatePickerComponent = () => {
+const DatePickerComponent = (props) => {
 	const [date, changeDate] = useState(new Date());
 
 	// prettier-ignore
 	return (
-
-     <MuiPickersUtilsProvider utils={DateFnsUtils}> <DatePicker
+    <Dialog open={props.open} onClose={props.onClose} > <MuiPickersUtilsProvider utils={DateFnsUtils}> <DatePicker
         autoOk
         variant="static"
         openTo="year"
         value={date}
         onChange={changeDate}
-      /></MuiPickersUtilsProvider>
+      /></MuiPickersUtilsProvider></Dialog>
+    
 
   );
 };
